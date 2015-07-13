8e2742f07d9700720119ef70ff0588c6e0148d55
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-07-13 14:39:45 -0400

    fixed jquery so only the question current is being edited

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,10 @@
 
 $(function(){ $(document).foundation(); });
 
-
 $(document).ready(function(){ 
-
+  $(".question_text_edit").toggle();
+  $(".question_type_edit").toggle();
+  
 	$( "#survey_form" ).submit(function( event ) {
 			event.preventDefault();
 			var title = $('#survey_title_edit').val();
@@ -52,20 +53,21 @@ $(document).ready(function(){
 	});
 
 
-	$( "#question_text_button_edit" ).click(function() {
-  	$("#question_text_edit").toggle();
-  	$("#question_type_edit").toggle();
-  	$("#question_text_solid").toggle();
-  	$("#question_type_solid").toggle();
-  	$("#question_text_button_confirm").toggle();
-  	$("#question_text_button_edit").toggle();
+	$( ".question_text_button_edit" ).click(function() {
+		var form = $(this).parent();
+  	$( form ).find(".question_text_edit").toggle();
+  	$( form ).find(".question_type_edit").toggle();
+  	$( form ).find(".question_text_solid").toggle();
+  	$( form ).find(".question_type_solid").toggle();
+  	$( form ).find(".question_text_button_confirm").toggle();
+  	$( form ).find(".question_text_button_edit").toggle();
 	});
 
 
-	$( "#question_form" ).submit(function( event ) {
+	$( ".question_form" ).submit(function( event ) {
 			event.preventDefault();
-			var text = $('#question_text_edit').val();
-			var question_type = $('#question_type_edit').val();
+			var text = $('.question_text_edit').val();
+			var question_type = $('.question_type_edit').val();
 		  var valuesToSubmit = $(this).serialize();
 		    $.ajax({
 		        type: "POST",
@@ -74,14 +76,14 @@ $(document).ready(function(){
 		        dataType: "html" // you want a difference between normal and ajax-calls, and json is standard
 		    }).success(function(data){
 		    });
-		$("#question_text_edit").toggle();
-  	$("#question_type_edit").toggle();
-  	$("#question_text_solid").toggle();
-  	$("#question_type_solid").toggle();
-  	$("#question_text_button_confirm").toggle();
-  	$("#question_text_button_edit").toggle();
-  	$('#question_text_solid').html( text );
-    $('#question_type_solid').html( question_type );
+		$( this ).find(".question_text_edit").toggle();
+  	$( this ).find(".question_type_edit").toggle();
+  	$( this ).find(".question_text_solid").toggle();
+  	$( this ).find(".question_type_solid").toggle();
+  	$( this ).find(".question_text_button_confirm").toggle();
+  	$( this ).find(".question_text_button_edit").toggle();
+  	$( this ).find('.question_text_solid').html( text );
+    $( this ).find('.question_type_solid').html( question_type );
 	});
 });
 
